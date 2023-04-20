0538e856840aa97ab7594d84d9b573eeb8f64604
ruturajghatage <ruturajghatage33@gmail.com>
2023-04-20 18:50:39 +0530

    Added the see my admins screen

--- lib/features/login/admin_widgets/all_users.dart
+++ lib/features/login/admin_widgets/all_users.dart
@@ -1,6 +1,13 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final storage = const FlutterSecureStorage();
 
 class UsersScreen extends StatefulWidget {
   const UsersScreen({Key? key}) : super(key: key);
@@ -10,8 +17,69 @@ class UsersScreen extends StatefulWidget {
 }
 
 class _UsersScreenState extends State<UsersScreen> {
+  late Future<List> users;
+
+  Future<List> getUsers() async {
+    String? token = await storage.read(key: 'admin_access_token');
+
+    final Dio dio = Dio();
+    dio.options.headers['Authorization'] = 'Bearer $token';
+
+    Response response =
+        await dio.get("https://client-hive.onrender.com/api/admin/users");
+
+    log(response.toString());
+    log(response.data.toString());
+
+    return response.data;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    users = getUsers();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: FutureBuilder(
+      future: users,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        print(snapshot.data);
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Error'),
+          );
+        }
+
+        if (snapshot.hasData && snapshot.data != null) {
+          if (snapshot.data.length == 0) {
+            return const Center(
+              child: Text('No users found'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {},
+                child: ListTile(
+                  title: Text(snapshot.data[index]['display_name']),
+                  subtitle: Text(snapshot.data[index]['email']),
+                ),
+              );
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    ));
   }
 }
